f4ccf676b3b41a0b643712de8cca3dfad0045622
ohsuz <gl_gd@naver.com>
2021-10-09 23:50:46 +0000

    done: multi-head attention

--- dekorde/components/mha.py
+++ dekorde/components/mha.py
@@ -1,4 +1,5 @@
 import torch
+import torch.nn as nn
 
 
 class MultiHeadAttentionLayer(torch.nn.Module):
@@ -12,23 +13,43 @@ class MultiHeadAttentionLayer(torch.nn.Module):
         self.heads = heads
         # any layers to optimise? - four linear layers in total.
         # TODO - define the shape of the weights.
-        self.W_q = torch.nn.Linear(..., ...)
-        self.W_k = torch.nn.Linear(..., ...)
-        self.W_v = torch.nn.Linear(..., ...)
-        self.W_o = torch.nn.Linear(..., ...)  # for aggregating the multi-head outputs.
+        self.head_dim = embed_size // heads
+        self.W_q = nn.Linear(self.head_dim, self.head_dim)
+        self.W_k = nn.Linear(self.head_dim, self.head_dim)
+        self.W_v = nn.Linear(self.head_dim, self.head_dim)
+        self.W_o = nn.Linear(self.embed_size, self.embed_size)  # for aggregating the multi-head outputs.
 
     def forward(self, EP_q: torch.Tensor, EP_k: torch.Tensor, EP_v: torch.Tensor, M: torch.Tensor = None) -> torch.Tensor:
         """
         :param EP_q: (N, L, E)
         :param EP_k: (N, L, E)
         :param EP_v: (N, L, E)
-        :param M: (???) The mask.
-        :return: H_all (N, L, H)
+        :param M: ???
+        :return: out (N, L, E)
         """
+        # Reshape all matrices
+        # (N, L, E) -> (N, L, heads, head_dim)
+        N = EP_q.shape[0]
+        q_len, k_len, v_len = EP_q.shape[1], EP_k.shape[1], EP_v.shape[1]
+        EP_q = torch.reshape(N, q_len, self.heads, self.head_dim)
+        EP_k = torch.reshape(N, k_len, self.heads, self.head_dim)
+        EP_v = torch.reshape(N, v_len, self.heads, self.head_dim)
+        
+        # Pass h linear layers
+        # (N, ?_len, heads, head_dim) -> (N, ?_len, heads, head_dim)
         Q = self.W_q(EP_q)
         K = self.W_k(EP_k)
         V = self.W_v(EP_v)
-        return self.scaled_dot_product_attention(Q, K, V, M)
+        
+        # Scaled Dot-Product Attention
+        # (N, ?_len, heads, head_dim) -> (N, q_len, embed_size)
+        attention = self.scaled_dot_product_attention(Q, K, V, M)
+        
+        # Pass a linear layer
+        # (N, q_len, embed_size) -> (N, q_len, embed_size)
+        out = self.W_o(attention)
+        
+        return out
 
     def scaled_dot_product_attention(self,
                                      Q: torch.Tensor,
@@ -36,16 +57,30 @@ class MultiHeadAttentionLayer(torch.nn.Module):
                                      V: torch.Tensor,
                                      M: torch.Tensor = None) -> torch.Tensor:
         """
-        :param Q: (N, L, H)
-        :param K: (N, L, H)
-        :param V: (N, L, H)
-        :param M: (???)
-        :return: H_all (N, L, H)
+        :param Q: (N, L, heads, head_dim)
+        :param K: (N, L, heads, head_dim)
+        :param V: (N, L, heads, head_dim)
+        :param M: ??
+        :return: out (N, L, E)
         """
-        # TODO
-        ...
-        if M:  # if not None.
-            ...
-        ...
-        raise NotImplementedError
-
+        N, L = Q.shape[0], Q.shape[1]
+        
+        # Matrix Multiplication : Q & K
+        attention = torch.einsum("nqhd,nkhd->nhqk", [Q, K])
+        
+        # Scaled
+        attention = attention / (self.embed_size ** (1/2))
+        
+        # Mask (Optional)
+        if M:
+            attention = attention.masked_fill(M == 0, float("-1e20"))
+        
+        # Softmax
+        attention = torch.softmax(attention, dim=3)
+        
+        # Matrix Multiplication : attention & V
+        out = torch.einsum("nhql,nlhd->nqhd", [attention, V]).reshape(
+            N, L, self.heads*self.head_dim
+        )
+        
+        return out
